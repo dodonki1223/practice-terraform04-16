f0f036a30a6f60e39fb4b09a8dd6e90af673edf3
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-05-23 21:14:55 +0900

    :sparkles: 2-2：基本操作 - リソースの更新 - 2
    - タグ情報を削除
    - ユーザーデータを使用して Apache のインストールと起動を行う

--- practice_terrafrom/02/2-1/main.tf
+++ practice_terrafrom/02/2-1/main.tf
@@ -9,7 +9,10 @@ resource "aws_instance" "example" {
     ami           = "ami-0c3fd0f5d33134a76"
     instance_type = "t3.micro"
 
-    tags = {
-        Name = "example"
-    }
+    // ユーザーデータを使用して Apache のインストールを行っている
+    user_data = <<EOF
+        #!/bin/bash
+        yum install -y httpd
+        systemctl start httpd.service
+EOF
 }
