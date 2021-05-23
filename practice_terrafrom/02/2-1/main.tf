a7c3d4aa20b6c3839c9c162379c24ffc1d35f5c3
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-05-23 21:06:39 +0900

    :sparkles: 2-1：基本操作 - リソースの作成

---
+++ practice_terrafrom/02/2-1/main.tf
@@ -0,0 +1,11 @@
+// AWSを使用するクレデンシャル情報をここで記述しておく
+// なぜか profile を指定しても region の設定が効かなかったので追加しておく
+provider "aws" {
+    region  = "ap-northeast-1"
+    profile = "terraform"
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-0c3fd0f5d33134a76"
+    instance_type = "t3.micro"
+}
