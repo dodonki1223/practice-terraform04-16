704c96c0070bb6b05868bdd742749c45a3dddfc8
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-06-09 09:18:33 +0900

    :sparkles: 10-2：ECS Scheduled Tasks - CloudWatchイベントIAMロール

--- practice_terrafrom/05-16/batch.tf
+++ practice_terrafrom/05-16/batch.tf
@@ -49,5 +49,21 @@ resource "aws_ecs_task_definition" "practice_terrafrom_batch" {
     network_mode             = "awsvpc"
     requires_compatibilities = ["FARGATE"]
     container_definitions    = file("./batch_container_definition.json")
-    // execution_role_arn       = module.ecs_task_execution_role.iam_role_arn
+    execution_role_arn       = module.ecs_task_execution_role.iam_role_arn
+}
+
+/*
+    CloudWatch イベントIAMロール
+        CloudWatch イベントからECSを起動するためのIAMロール
+ */
+module "ecs_events_role" {
+    source     = "./iam_role"
+    name       = "ecs-events"
+    identifier = "events.amazonaws.com"
+    policy     = data.aws_iam_policy.ecs_events_role_policy.policy
+}
+
+// AmazonEC2ContainerServiceEventsRole ポリシーを使用すると「タスクを実行する」権限と「タスクにIAMロールを渡す」権限を付与できる
+data "aws_iam_policy" "ecs_events_role_policy" {
+    arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceEventsRole"
 }
