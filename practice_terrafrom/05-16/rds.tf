dc7afa0961ed87b3439d24885ba85b7cda2ebe4e
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-06-13 16:15:43 +0900

    :sparkles: 13-1：データストア - RDS - DBオプショングループ

--- practice_terrafrom/05-16/rds.tf
+++ practice_terrafrom/05-16/rds.tf
@@ -21,3 +21,21 @@ resource "aws_db_parameter_group" "practice_terrafrom_rds_pg" {
         value = "utf8mb4"
     }
 }
+
+/*
+    DBオプショングループ
+        データベースエンジンにオプション機能を追加する
+ */
+resource "aws_db_option_group" "practice_terrafrom_rds_og" {
+    name                 = "practice-terrafrom-rds-og"
+    //「mysql」のようなエンジン名を設定する
+    engine_name          = "mysql"
+    //「5.7」のようなメジャーバージョンを設定する
+    major_engine_version = "5.7"
+
+    option {
+        // MariaDB監査プラグインを追加
+        // ユーザーのログオンや実行したクエリなどのアクティビティを記録するためのプラグイン
+        option_name = "MARIADB_AUDIT_PLUGIN"
+    }
+}
