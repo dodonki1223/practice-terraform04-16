7295ff3891698e43907edc83392a471c90585ef2
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-06-14 07:29:12 +0900

    :sparkles: 14-2：デプロイメントパイプライン - ECRライフサイクルポリシー

--- practice_terrafrom/05-16/ecr.tf
+++ practice_terrafrom/05-16/ecr.tf
@@ -4,3 +4,33 @@
 resource "aws_ecr_repository" "practice_terrafrom_ecr" {
     name = "practice-terrafrom-ecr"
 }
+
+/*
+    ECRライフサイクルポリシー
+        ECRリポジトリに保存できるイメージ数には限りがあります
+        詳しくは公式ドキュメントを参照すること：https://docs.aws.amazon.com/ja_jp/AmazonECR/latest/userguide/LifecyclePolicies.html
+ */
+resource "aws_ecr_lifecycle_policy" "practice_terrafrom_ecr_lcp" {
+    repository = aws_ecr_repository.practice_terrafrom_ecr.name
+
+    // 「release」ではじまるイメージタグを30個までに制限している
+    policy = <<EOF
+    {
+        "rules": [
+            {
+                "rulePriority": 1,
+                "description": "Keep last 30 release tagged images",
+                "selection": {
+                    "tagStatus": "tagged",
+                    "tagPrefixList": ["release"],
+                    "countType": "imageCountMoreThan",
+                    "countNumber": 30
+                },
+                "action": {
+                    "type": "expire"
+                }
+            }
+        ]
+    }
+EOF
+}
