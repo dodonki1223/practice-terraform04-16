fc2df385abbede74c4821d9b7edbafa245b841ef
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-06-14 08:50:09 +0900

    :sparkles: 14-2：デプロイメントパイプライン - Dockerイメージのプッシュ

--- practice_terrafrom/05-16/ecr.tf
+++ practice_terrafrom/05-16/ecr.tf
@@ -1,4 +1,13 @@
 /*
+    ECRへDockerイメージのプッシュ
+        aws --profile terraform ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 300367504550.dkr.ecr.ap-northeast-1.amazonaws.com
+        docker build -t 300367504550.dkr.ecr.ap-northeast-1.amazonaws.com/practice-terrafrom-ecr:latest ./hello-world/
+        docker push 300367504550.dkr.ecr.ap-northeast-1.amazonaws.com/practice-terrafrom-ecr:latest
+    詳しくは以下の公式ドキュメントを参考にするとよい
+        https://docs.aws.amazon.com/ja_jp/AmazonECR/latest/userguide/getting-started-cli.html
+ */
+
+/*
     ECRリポジトリ（Elastic Container Registry）
  */
 resource "aws_ecr_repository" "practice_terrafrom_ecr" {
