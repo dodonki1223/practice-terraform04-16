3e51c3b4149cfc9210447d17107728139a8ee955
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-06-14 09:40:19 +0900

    :sparkles: 14-3：デプロイメントパイプライン - 継続的インテグレーション - CodeBuildプロジェクト

--- practice_terrafrom/05-16/codebuild.tf
+++ practice_terrafrom/05-16/codebuild.tf
@@ -42,3 +42,35 @@ module "codebuild_role" {
     identifier = "codebuild.amazonaws.com"
     policy = data.aws_iam_policy_document.codebuild.json
 }
+
+/*
+    CodeBuildプロジェクト
+ */
+resource "aws_codebuild_project" "practice_terrafrom_cb_p" {
+    name         = "practice-terrafrom-cb-p"
+    // CodeBuild用のIAMロールを設定する
+    service_role = module.codebuild_role.iam_role_arn
+
+    // ビルド対象のファイルをsourceで指定する
+    source {
+        // CODEPIPELINEと指定することでCodePipelineと連携することを宣言する
+        type = "CODEPIPELINE"
+    }
+
+    // ビルド出力アーティファクトの格納先をartifactsで指定する
+    artifacts {
+        // CODEPIPELINEと指定することでCodePipelineと連携することを宣言する
+        type = "CODEPIPELINE"
+    }
+
+    // ビルド環境
+    environment {
+        type            = "LINUX_CONTAINER"
+        compute_type    = "BUILD_GENERAL1_SMALL"
+        // "aws/codebuild/standard:2.0"はAWSが管理しているUbuntuベースのイメージ
+        // このイメージを使う場合は「14.3.3ビルド仕様」でランタイムバージョンの指定が必要になります
+        image           = "aws/codebuild/standard:2.0"
+        //  ビルド時にDockerコマンドを使用するため、privileged_modeをtrueにして、特権を付与する
+        privileged_mode = true
+    }
+}
