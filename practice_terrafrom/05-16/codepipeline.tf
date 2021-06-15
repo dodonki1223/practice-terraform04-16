57e2649db1873d16a61159d0fd236c6fa9d532c1
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-06-15 11:31:41 +0900

    :sparkles: 14-4：デプロイメントパイプライン - 継続的デリバリー - CodePipeline

--- practice_terrafrom/05-16/codepipeline.tf
+++ practice_terrafrom/05-16/codepipeline.tf
@@ -37,3 +37,92 @@ module "codepipeline_role" {
     identifier = "codepipeline.amazonaws.com"
     policy     = data.aws_iam_policy_document.codepipeline.json
 }
+
+/*
+    CodePipeline
+ */
+resource "aws_codepipeline" "practice_terrafrom_cp" {
+    name     = "practice-terrafrom-cp"
+    role_arn = module.codebuild_role.iam_role_arn
+
+    /*
+        Sourceステージ：GitHubからソースコードを取得する
+            GitHubリポジトリとブランチを指定する
+            CodePipelineの起動はWebhookで行うためPollForSourceChangesはfalseにしてポーリングを無効にする
+     */
+    stage {
+        name = "Source"
+
+        action {
+            name             = "Source"
+            category         = "Source"
+            owner            = "ThirdParty"
+            provider         = "GitHub"
+            version          = 1
+            output_artifacts = ["Source"]
+
+            configuration = {
+                Owner                = "dodonki1223"
+                Repo                 = "terraform-study"
+                Branch               = "main"
+                PollForSourceChanges = false
+            }
+        }
+    }
+
+    /*
+        Buildステージ：CodeBuildを指定する
+     */
+    stage {
+        name = "Build"
+
+        action {
+            name             = "Build"
+            category         = "Build"
+            owner            = "AWS"
+            provider         = "CodeBuild"
+            version          = 1
+            input_artifacts  = ["Source"]
+            output_artifacts = ["Build"]
+
+            configuration = {
+                ProjectName = aws_codebuild_project.practice_terrafrom_cb_p.id
+            }
+        }
+    }
+
+    /*
+        Deployステージ：デプロイ先のECSクラスとECSサービスを指定する
+     */
+    stage {
+        name = "Deploy"
+
+        action {
+            name             = "Deploy"
+            category         = "Deploy"
+            owner            = "AWS"
+            provider         = "ECS"
+            version          = 1
+            input_artifacts  = ["Build"]
+
+            /*
+                imagedefinitions.json
+                    name で指定したコンテナを、imageUriに指定したイメージで更新します
+                    ECS Fargateの場合、latest タグでも必ずdocker pullするため、デプロイ
+                    ごとにタグを変更する必要がありません
+                    name に指定するのは：aws_ecs_task_definition
+                    imageUriにしてするのは：aws_ecs_task_definitionのfamily
+             */
+            configuration = {
+                ClusterName = aws_ecs_cluster.practice_terrafrom_ecs.name 
+                ServiceName = aws_ecs_service.practice_terrafrom_ecs_service.name
+                FileName    = "imagedefinitions.json"
+            }
+        }
+    }
+
+    artifact_store {
+        location = aws_s3_bucket.artifact.id
+        type     = "S3"
+    }
+}
