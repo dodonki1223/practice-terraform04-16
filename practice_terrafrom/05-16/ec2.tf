d5e0a4f924136771f394eb951684f71a1d9a55f0
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-06-20 00:13:50 +0900

    :sparkles: 15-2 オペレーションサーバーの設計 - Session Manager - ポリシードキュメント

--- practice_terrafrom/05-16/ec2.tf
+++ practice_terrafrom/05-16/ec2.tf
@@ -11,4 +11,45 @@
         ＜トレーサビリティ＞
             同じくSession Managerで、すべての操作ログを保存する
             コマンドの実行結果も自動的に残し、トレーサビリティを確保します
+        ＜Session Manager＞
+            SSHログインなしに、シェルアクセスを実現するサービスです
+            専用のエージェントをインストールして、そのエージェント経由でコマンドを実行する
+            Session Managerでは実際にログインすることなく、ログインしているかのようにオペレーションできる
+            Amazon Linux2であれば、標準でエージェントがインストールされている
+        ＜インスタンスプロファイル＞
+            EC2は特殊で、直接IAMロールを関連付けできません
+            かわりに、IAMロールをラップしたインスタンスプロファイルを関連付けて権限を付与する
  */
+
+/*
+    ポリシードキュメント
+ */
+data "aws_iam_policy_document" "ec2_for_ssm" {
+    source_json = data.aws_iam_policy.ec2_for_ssm.policy
+
+    statement {
+        effect    = "Allow"
+        resources = ["*"]
+
+        // AmazonSSMManagedInstanceCore ポリシーをベースにし、S3バケットとCloudWatch Logsへの書き込み権限を付与する
+        // SSMパラメータストアとECRへの参照権限を追加することでEC2上で「ECRに格納したイメージのdocker pull」と「SSM
+        // パラメータストアから設定情報を注入したコンテナの起動」を実現できる
+        actions = [
+            "s3:PutObject",
+            "logs:PutLogEvents",
+            "logs:CreateLogStream",
+            "ecr:GetAuthorizationToken",
+            "ecr:BatchCheckLayerAvailability",
+            "ecr:GetDownloadUrlForLayer",
+            "ecr:BatchGetImage",
+            "ssm:GetParameter",
+            "ssm:GetParameters",
+            "ssmGetParametersByPath",
+            "kms:Decrypt",
+        ]
+    }
+}
+
+data "aws_iam_policy" "ec2_for_ssm" {
+    arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
