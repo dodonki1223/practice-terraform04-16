73133d7c1be3f7303b88b95c39edad8c257705bc
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-06-23 07:13:34 +0900

    :recycle: GitHub の Provider の書きぶりが変更されていたので変更する

--- practice_terrafrom/05-16/main.tf
+++ practice_terrafrom/05-16/main.tf
@@ -1,3 +1,12 @@
+terraform {
+    required_providers {
+        github = {
+            source  = "integrations/github"
+            version = "~> 4.0"
+        }
+    }
+}
+
 // 実行するアカウント情報
 provider "aws" {
     region  = "ap-northeast-1"
