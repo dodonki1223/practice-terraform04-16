03149fac4fcf4c47ed52136be28eba2fc97c080e
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-06-30 10:06:08 +0900

    :sparkles: 16-3 ロギング - ログ永続化 - Kinesis Data Firehose - ポリシードキュメント

---
+++ kinesis.tf
@@ -0,0 +1,23 @@
+/*
+    ポリシードキュメント
+        Kinesis Data FirehoseにS3の操作権限を付与する
+ */
+data "aws_iam_policy_document" "kinesis_data_firehose" {
+    statement {
+        effect = "Allow"
+
+        actions = [
+            "s3:AbortMultipartUpload",
+            "s3:GetBucketLocation",
+            "s3:GetObject",
+            "s3:ListBucket",
+            "s3:ListBucketMultipartUploads",
+            "s3:PutObject",
+        ]
+
+        resources = [
+            "arn:aws:s3:::${aws_s3_bucket.cloudwatch_logs.id}",
+            "arn:aws:s3:::${aws_s3_bucket.cloudwatch_logs.id}/*"
+        ]
+    }
+}
